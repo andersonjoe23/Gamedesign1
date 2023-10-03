6f8090222f22144ef82262b2e0e5eca5c5fe55d1
andersonjoe23 <144834267+andersonjoe23@users.noreply.github.com>
2023-10-03 12:29:35 -0500

    update

---
+++ Scoreboard.gd
@@ -0,0 +1,31 @@
+extends Control
+
+var score = 0
+
+func addpoint(): score += 1
+func subpoint(): score +=1
+func reset(): score = 0
+func update():
+	$lblout.text = "score: %d" & score
+	
+	
+func _on_btn_update_pressed():
+	var choice = $lineEdit.text.to_lower()
+	if choice == "add":
+		addpoint()
+	elif choice == "sub" or choice == "subtract":
+		subpoint()
+	else:
+		OS.alert("invalid choice!")
+	update()
+		
+		
+	
+func _on_btnreset_pressed():
+	reset()
+	update()
+
+
+
+
+
