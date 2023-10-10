dd9bc2e9511427ceb58928251b5c80dc3ada89c8
andersonjoe23 <144834267+andersonjoe23@users.noreply.github.com>
2023-10-10 12:42:41 -0500

    update

--- kbd_ctrl.gd
+++ kbd_ctrl.gd
@@ -9,3 +9,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_interact_mouse_entered():
+	if input.is_mouse_butten_pressed(Mouse_Butten_Left):
+		$interact.queue_freez()
+var dragging = false
+var offset = Vector2.ZERO
+
+func _on_button_button_down():
+	dragging = true
+	offset = get_global_mouse_position() - $draggable.global_position
+
+func _on_button_button_up():
+	dragging = false
+
+func _process(delta):
+	if dragged():
+		$draggable..global_posstion = get_global_mouse_position() - offset
